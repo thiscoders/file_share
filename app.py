b5dcea670b57a8da7eb3ff63f1f1addcc7575e42
liuye <ye.liu01@hand-china.com>
2020-04-28 17:04:01 +0800

    文件下载初始化

---
+++ app.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import os
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+def make_tree(path='static'):
+    tree = dict(name=path, children=[])
+    try:
+        lst = os.listdir(path)
+    except OSError:
+        print("error...")
+    else:
+        for name in lst:
+            fn = os.path.join(path, name)
+            if os.path.isdir(fn):
+                tree_sub = make_tree(fn)
+                tree['children'].append(tree_sub)
+            else:
+                tree['children'].append(dict(name=name, path=fn))
+    return tree
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        password = request.form.get('password')
+        if password != '1':
+            return render_template('login.html', error_msg='口令错误！')
+        else:
+            tree = make_tree()
+            return render_template('tree.html', tree=tree)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
